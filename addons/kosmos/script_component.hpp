e2461d89aaaa862d79eee247666542e05277bfa4
OverlordZorn <dennisAzorn@gmail.com>
2026-03-19 14:12:29 +0100

    fix component casing

--- addons/kosmos/script_component.hpp
+++ addons/kosmos/script_component.hpp
@@ -1,4 +1,4 @@
-#define COMPONENT Kosmos
+#define COMPONENT kosmos
 
 // This is being used for the Addon's Name and can be "My Addon Template Framework"
 #define COMPONENT_BEAUTIFIED Kosmos Cigarettes
